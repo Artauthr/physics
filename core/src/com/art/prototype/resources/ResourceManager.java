de65e0d78474c509f8c909095d037892027616ee
Artauthr <arthurthkng@gmail.com>
2023-12-29 15:07:50 +0400

    resources

--- core/src/com/art/prototype/resources/ResourceManager.java
+++ core/src/com/art/prototype/resources/ResourceManager.java
@@ -1,4 +1,31 @@
 package com.art.prototype.resources;
 
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import lombok.Getter;
+
 public class ResourceManager {
+    private AssetManager assetManager;
+
+    @Getter
+    private Skin uiSkin;
+
+    private TextureAtlas atlas;
+
+    public ResourceManager () {
+        assetManager = new AssetManager();
+
+    }
+
+    private void load () {
+        assetManager.update();
+    }
+
+    private void finishLoading () {
+        this.atlas = assetManager.get("gameassets/gameatlas.atlas", TextureAtlas.class);
+
+        this.uiSkin = new Skin(atlas);
+    }
 }
